59cf1964851c8e38129fa40d508d7b1145349d0a
Rybtsov <rybtsov@gmail.com>
2023-08-30 18:51:37 +0500

    добавлено заполнение списка предметов

--- 01_dbinit.sql
+++ 01_dbinit.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS public.subject
     CONSTRAINT subject_pkey PRIMARY KEY (subject_id)
 );
 ALTER TABLE public.subject ADD COLUMN IF NOT EXISTS subject_name text;
-ALTER TABLE public.subject ADD COLUMN IF NOT EXISTS subject_parent text;
+ALTER TABLE public.subject ADD COLUMN IF NOT EXISTS subject_parent_name text;
 ALTER TABLE public.subject ADD COLUMN IF NOT EXISTS subject_full_name text;
 ALTER TABLE public.subject ADD COLUMN IF NOT EXISTS subject_goods_raw json;
--- 02_sync_subjects_list.py
+++ 02_sync_subjects_list.py
@@ -1,6 +1,35 @@
 import requests
-
+import config
+import psycopg
 
 url = 'https://static-basket-01.wb.ru/vol0/data/subject-base.json'
-r  = requests.get(url)
-print(r.text)
+r = requests.get(url)
+subjects = []
+
+for parent in r.json():
+    for child in parent['childs']:
+        item = {
+            'subject_id': child['id'],
+            'subject_name': child['name'],
+            'subject_full_name': f"{parent['name']} \ {child['name']}",
+            'subject_parent_name': parent['name'],
+        }
+        subjects.append(item)
+
+cols = ', '.join(list(subjects[0].keys()))
+placeholders = ', '.join(['%s' for item in list(subjects[0].keys())])
+i = 0
+
+with psycopg.connect(host=config.DB_HOST,
+                     port=config.DB_PORT,
+                     dbname=config.DB_BASE,
+                     user=config.DB_USER,
+                     password=config.DB_PASS) as conn:
+    with conn.cursor() as cur:
+        for subject in subjects:
+            values = list(subject.values())
+            sql = f'INSERT INTO subject ({cols}) VALUES ({placeholders}) ON CONFLICT DO NOTHING'
+            cur.execute(sql, values)
+            i += 1
+            print(i, 'выполнено успешно')
+
